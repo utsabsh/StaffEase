9b366b06f909de41dd6310a969f8230dd4c573bc
utsabsh <np03cs4a220209@heraldcollege.edu.np>
2025-02-16 20:33:29 +0545

    admin and employee count

--- server/Routes/AdminRoutes.js
+++ server/Routes/AdminRoutes.js
@@ -220,6 +220,21 @@ router.put("/update_leave/:id", (req, res) => {
     return res.json({ status: true });
   });
 });
+router.get("/admin_count", (req, res) => {
+  const sql = "select count(id) as admin from admin";
+  con.query(sql, (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query Error" + err });
+    return res.json({ Status: true, Result: result });
+  });
+});
+
+router.get("/employee_count", (req, res) => {
+  const sql = "select count(id) as employee from employee";
+  con.query(sql, (err, result) => {
+    if (err) return res.json({ Status: false, Error: "Query Error" + err });
+    return res.json({ Status: true, Result: result });
+  });
+});
 
 router.get("/logout", (req, res) => {
   res.clearCookie("token");
